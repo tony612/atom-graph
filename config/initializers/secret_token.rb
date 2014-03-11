4d9abd1e050178c1bc7fbe88df9604ad6ffa85af
Tony Han <h.bing612@gmail.com>
2014-03-11 11:11:04 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AtomGraph::Application.config.secret_key_base = '8f2dde0d25eb126253cde8ef108d875c696410494132b42ebb1d330a2d3a2664a23ff3fd0bc33bb3f369870a662753ea247c7de728d57f086358896f8b68efcb'
